aaaf8fa1cafc0af9d141cec6dcb4fabbf9bd2ec8
root <root@ubuntu-cis>
2024-07-08 11:00:43 +0000

    modified install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ running_dockers=$(cat .env | grep RUNNING_DOCKERS | awk -F '=' '{print $2}')
 if [ "$running_dockers" != "" ]; then
 	docker-compose up -d $running_dockers
 fi
+
+if [ -d shuffle-database ]; then
+	chown -R 1000:1000 shuffle-database
+fi	
